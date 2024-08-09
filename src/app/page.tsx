1ea35ea09a2c48d32c8bbf4611defbe49f3443df
Hyunkyu Jang <140380514+HyunkyuJang@users.noreply.github.com>
2024-08-09 09:54:06 +0900

    UI 조정.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,19 @@ import _ from "lodash";
 import {For} from "react-loops";
 import {FlickrImageData} from "@/data/imageData";
 
+function LabelText({label,value}:{label:string,value:string|number}) {
+    return (
+        <div className={"grid grid-cols-4 items-center space-x-2"}>
+            <span className={"col-span-1 w-20"}>{label}</span>
+            <span className={"col-span-3 text-gray-600 text-sm overflow-hidden text-nowrap text-ellipsis"}>{value}</span>
+        </div>
 
-export default function HomePage(){
-    const [searchText,setSearchText] = useState("");
+)
+
+}
+
+export default function HomePage() {
+    const [searchText, setSearchText] = useState("");
     const dataList = useMemo(()=>{
         if(!searchText){
             const randomItems:CollDataType[] = _.sampleSize(CollData, 10);
@@ -31,8 +41,8 @@ export default function HomePage(){
                 <For of={dataList}>
                     {(data)=>{
                         return (
-                            <div className={"flex flex-col justify-end items-center p-4 border rounded-2xl  "}>
-                                <div className="grid grid-cols-2 gap-3 h-64 overflow-hidden">
+                            <div className={"flex flex-col justify-end items-center p-4 border rounded-2xl h-64 "}>
+                                <div className="grid grid-cols-2 gap-3 h-64 overflow-y-scroll w-full">
                                     <For of={FlickrImageData.filter((item) => {
                                         return item.fileName.startsWith(data.id)
                                     }).slice(0, 4)}>
@@ -41,19 +51,13 @@ export default function HomePage(){
                                         }}
                                     </For>
                                 </div>
-                                <div className={"grid grid-cols-4 w-full justify-start items-start gap-4"}>
-                                    <span>ID</span>
-                                    <span className={"text-gray-600 text-sm"}>{data.id}</span>
-                                    <span>Name</span>
-                                    <span className={"text-gray-600 text-sm"}>{data.nameKr}</span>
-                                    <span>Type</span>
-                                    <span className={"text-gray-600 text-sm"}>{data.type}</span>
-                                    <span>Location</span>
-                                    <span className={"text-gray-600 text-sm"}>{data.location}</span>
-                                    <span>Date</span>
-                                    <span className={"text-gray-600 text-sm"}>{data.collDate}</span>
-                                    <span>Host</span>
-                                    <span className={"text-gray-600 text-sm"}>{data.host}</span>
+                                <div className={"grid grid-cols-2 w-full justify-start items-start gap-2"}>
+                                    <LabelText label={"ID"} value={data.id}/>
+                                    <LabelText label={"Name"} value={data.nameKr}/>
+                                    <LabelText label={"Type"} value={data.type}/>
+                                    <LabelText label={"Loc."} value={data.location}/>
+                                    <LabelText label={"Date"} value={data.collDate}/>
+                                    <LabelText label={"Host"} value={data.host}/>
                                 </div>
 
 
